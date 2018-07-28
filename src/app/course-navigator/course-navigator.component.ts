1671658050538847781382cc77137c966a007f85
Annie <annienero93@gmail.com>
2018-07-28 17:34:40 -0400

    course grid

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -11,14 +11,28 @@ export class CourseNavigatorComponent implements OnInit {
   constructor(private service: CourseNavigatorServiceClient) { }
 
   courses = []
+  modules = []
+  lessons = []
+  widgets = []
 
   ngOnInit() {
     this.service.findAllCourses()
       .then(courses => this.courses = courses)
   }
 
-  viewCourse(courseId) {
-    alert("viewing course " + courseId)
-}
+  selectCourse(courseId) {
+    this.service.findAllModulesForCourse(courseId)
+      .then(modules => this.modules = modules)
+  }
+
+  selectModule(moduleId) {
+    this.service.findAllLessonsForModule(moduleId)
+      .then(lessons => this.lessons = lessons)
+  }
+
+  selectLesson(lessonId) {
+    this.service.findAllWidgetsForLesson(lessonId)
+      .then(widgets => this.widgets = widgets)
+  }
 
 }
--- src/app/services/course-navigator.service.client.ts
+++ src/app/services/course-navigator.service.client.ts
@@ -6,4 +6,19 @@ export class CourseNavigatorServiceClient {
         return fetch("http://localhost:8080/api/course")
             .then(response => response.json())
     }
+
+    findAllModulesForCourse(courseId) {
+        return fetch("http://localhost:8080/api/course/" + courseId + "/module")
+            .then(response => response.json())
+    }
+
+    findAllLessonsForModule(moduleID) {
+        return fetch("http://localhost:8080/api/course/0/module/" + moduleID + "/lesson")
+            .then(response => response.json())
+    }
+
+    findAllWidgetsForLesson(lessonId) {
+        return fetch("http://localhost:8080/api/lesson/" + lessonId + "/widget")
+            .then(response => response.json())
+    }
 }
