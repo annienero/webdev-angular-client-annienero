a87aaaa3d3ee599c4d0e2d32e460e390cc99a3f6
Annie <annienero93@gmail.com>
2018-07-29 20:56:22 -0400

    update course viewer

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,11 @@
 import {Routes, RouterModule } from '@angular/router'
 import {CourseManagerComponent} from './course-manager/course-manager.component'
+import { CourseViewerComponent } from './course-viewer/course-viewer.component';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: CourseManagerComponent},
+    {path: 'course/:courseId', component: CourseViewerComponent},
     {path: '**', component: CourseManagerComponent}
 ]
 
