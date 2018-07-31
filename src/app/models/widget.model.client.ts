502f0ae85913bb5094b4d5fdab03367e81e80c88
Annie <annienero93@gmail.com>
2018-07-31 11:53:30 -0400

    render widget types

--- src/app/models/widget.model.client.ts
+++ src/app/models/widget.model.client.ts
@@ -1,4 +1,11 @@
 export class Widget {
     id: number
     name: string
+    widgetOrder: number
+    text: string
+    className: string
+    src: string
+    href: string
+    listType: string
+    size: number
 }
