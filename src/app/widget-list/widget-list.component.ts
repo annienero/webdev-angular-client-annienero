7dd113a5cadc3b422619dd795fb024b32abfde78
Annie <annienero93@gmail.com>
2018-07-31 10:48:39 -0400

    render widget titles on tabs

--- src/app/widget-list/widget-list.component.ts
+++ src/app/widget-list/widget-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '../../../node_modules/@angular/router';
+import { WidgetServiceClient } from '../services/widget.service.client';
+import { Widget } from '../models/widget.model.client';
 
 @Component({
   selector: 'app-widget-list',
@@ -7,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WidgetListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: WidgetServiceClient, private route: ActivatedRoute) { 
+    this.route.params.subscribe(params => this.setParams(params))
+  }
+
+  context
+  lessonId
+  widgets: Widget[] = []
+  setParams(params) {
+    this.context = params
+    this.lessonId = params.lessonId
+    this.loadWidgets(params.lessonId)
+  }
+
+  loadWidgets(lessonId) {
+    this.service.findAllWidgetsForLesson(lessonId) 
+      .then(widgets => this.widgets = widgets)
+  }
 
   ngOnInit() {
   }
