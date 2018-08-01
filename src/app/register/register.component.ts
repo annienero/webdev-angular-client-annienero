fff6806d31105e0c7e4872630c8951ca1fac1a34
Annie <annienero93@gmail.com>
2018-08-01 11:24:36 -0400

    register working

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserServiceClient } from '../services/user.service.client';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: UserServiceClient, private router: Router) { }
+
+  username
+  password
+  password2
+
+  register(username, password, password2) {
+    if (password !== password2) {
+      alert("Passwords do not match!")
+      return
+    }
+    if (username == null) {
+      alert("Enter a username")
+      return
+    }
+    if (password == null) {
+      alert("Enter a password")
+      return
+    }
+    this.service.createUser(username, password)
+      .then((()=> this.router.navigate(['home']))) //TODO eventually go to profile
+  }
 
   ngOnInit() {
   }
