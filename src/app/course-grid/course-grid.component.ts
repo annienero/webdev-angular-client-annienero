79168617552a989f00a822afc0ee2baf54520084
Annie <annienero93@gmail.com>
2018-08-06 14:03:50 -0400

    show logged in user sections

--- src/app/course-grid/course-grid.component.ts
+++ src/app/course-grid/course-grid.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CourseServiceClient } from '../services/course.service.client';
 import { Course } from '../models/course.model.client'
+import { UserServiceClient } from '../services/user.service.client';
+import { SectionServiceClient } from '../services/section.service.client';
+import { Section } from '../models/section.model.client';
 
 @Component({
   selector: 'app-course-grid',
@@ -9,12 +12,20 @@ import { Course } from '../models/course.model.client'
 })
 export class CourseGridComponent implements OnInit {
 
-  constructor(private service: CourseServiceClient) { }
+  constructor(private service: CourseServiceClient, 
+              private userService: UserServiceClient,
+              private sectionService: SectionServiceClient) { }
 
   courses: Course[] = []
+  sections: Section[] = []
+  user = false
 
   ngOnInit() {
+    this.userService.profile()
+      .then(() => { this.user = true })
     this.service.findAllCourses()
       .then(courses => this.courses = courses)
+    this.sectionService.findSectionsForStudent()
+      .then(sections => this.sections = sections)
   }
 }
