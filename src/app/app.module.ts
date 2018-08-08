a5d53cb46f43e4c0428597198120127c03a48fc9
Annie <annienero93@gmail.com>
2018-08-08 17:44:27 -0400

    add new questions to app.mod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,10 @@ import { SectionServiceClient } from './services/section.service.client';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { QuizListComponent } from './quiz-list/quiz-list.component';
 import { QuizTakerComponent } from './quiz-taker/quiz-taker.component';
+import { TrueFalseQuestionComponent } from './true-false-question/true-false-question.component';
+import { FillBlanksQuestionComponent } from './fill-blanks-question/fill-blanks-question.component';
+import { MultipleChoiceQuestionComponent } from './multiple-choice-question/multiple-choice-question.component';
+import { EssayQuestionComponent } from './essay-question/essay-question.component';
 
 @NgModule({
   declarations: [
@@ -43,7 +47,11 @@ import { QuizTakerComponent } from './quiz-taker/quiz-taker.component';
     SectionListComponent,
     AdminPageComponent,
     QuizListComponent,
-    QuizTakerComponent
+    QuizTakerComponent,
+    TrueFalseQuestionComponent,
+    FillBlanksQuestionComponent,
+    MultipleChoiceQuestionComponent,
+    EssayQuestionComponent
   ],
   imports: [
     BrowserModule,
