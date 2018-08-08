69dddd14ad05395addbd8e51ecb5ee5db81843c3
Annie <annienero93@gmail.com>
2018-08-08 17:45:14 -0400

    add submit quiz service

--- src/app/services/quiz.service.client.ts
+++ src/app/services/quiz.service.client.ts
@@ -4,13 +4,26 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class QuizServiceClient {
-    findAllQuizes() {
-        return fetch("http://localhost:3000/api/quiz")
+    findAllQuizzes() {
+        return fetch("http://localhost:3002/api/quiz")
             .then(response => response.json())
     }
 
     findQuizById(quizId) {
-        return fetch("http://localhost:3000/api/quiz/" + quizId)
+        return fetch("http://localhost:3002/api/quiz/" + quizId)
+        .then(response => response.json())
+    }
+
+    submitQuiz(quiz) {
+        return fetch("http://localhost:3002/api/quiz/" + quiz._id + "/submission", {
+            method: 'post',
+            body: JSON.stringify(quiz),
+            credentials: 'include',
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
         .then(response => response.json())
     }
 }
+
